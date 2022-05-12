9fd8491ece946b1fb8f0770bd58fa595903a5597
ananopaisdojavascript <75840630+ananopaisdojavascript@users.noreply.github.com>
2022-05-12 14:43:47 -0300

    Conversão de tipos - aula 04-04

--- CJRM/aulas/app.js
+++ CJRM/aulas/app.js
@@ -166,3 +166,22 @@ console.log(age === '31')
 
 console.log(age !== 31)
 console.log(age !== '31')
+
+// conversão de tipos
+
+let score = '100'
+score = Number(score)
+console.log(score + 1)
+
+console.log(typeof score)
+
+const crazyConversion = Number('Maçã')
+console.log(crazyConversion)
+
+const convertedNumber = String(97)
+console.log(convertedNumber)
+
+const booleanConversion = Boolean(10)
+console.log(booleanConversion)
+
+console.log(Boolean(0))
