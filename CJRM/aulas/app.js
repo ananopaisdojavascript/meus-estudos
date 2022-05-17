7393c7797487f32c0625e27e81edb634ed3f3761
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-05-17 18:17:31 -0300

    Arquivo atualizado

--- CJRM/aulas/app.js
+++ CJRM/aulas/app.js
@@ -88,3 +88,100 @@ const html = `
     <span>Esse post tem ${postComments} comentários.</span>
 `
 console.log(html)
+
+// arrays ou vetores
+
+let heroes = ['Batman', 'Catwoman', 'Iron Man']
+console.log(heroes[0])
+
+heroes[2] = 'Spider-Man'
+
+const ages = [31, 25, 39, 40, 25]
+console.log(ages[2])
+
+// métodos de arrays
+
+console.log(heroes.length)
+const joinHeroes = heroes.join(' , ')
+console.log(joinHeroes)
+
+const indexOf25 = ages.indexOf(25)
+console.log(indexOf25)
+
+const heroesConcat = heroes.concat(['Superman', 'Woverine'])
+console.log(heroesConcat)
+
+const pushToHeroes = heroes.push('Cyclops', 'Hulk')
+console.log(pushToHeroes)
+
+const popHeroes = heroes.pop()
+console.log(popHeroes)
+
+// null e undefined
+
+// let emptiness;
+// console.log(emptiness, emptiness + 3, `O valor é ${emptiness}`)
+
+let emptiness = null
+console.log(emptiness, emptiness + 3, `O valor é ${emptiness}`)
+
+// booleanos e comparações
+
+console.log(true, false)
+
+// métodos podem retornar booleanos
+
+const email = 'brucewayne@email.com.br'
+const includes = email.includes('@')
+console.log(includes)
+const names = ['dio', 'roger', 'robert']
+const arrayIncludes = names.includes('robert')
+console.log(arrayIncludes)
+
+// operadores de comparação
+
+const age = 31
+
+console.log(age == 31)
+console.log(age == 35)
+console.log(age != 35)
+console.log(age != 31)
+console.log(age > 30)
+console.log(age > 31)
+console.log(age < 31)
+console.log(age <= 31)
+console.log(age >= 31)
+
+const name = 'roger'
+console.log(name == 'roger')
+console.log(name == 'Roger')
+
+// exatamente igual a e exatamente diferente de
+
+console.log(age == 31)
+console.log(age == '31')
+
+console.log(age === 31)
+console.log(age === '31')
+
+console.log(age !== 31)
+console.log(age !== '31')
+
+// conversão de tipos
+
+let score = '100'
+score = Number(score)
+console.log(score + 1)
+
+console.log(typeof score)
+
+const crazyConversion = Number('Maçã')
+console.log(crazyConversion)
+
+const convertedNumber = String(97)
+console.log(convertedNumber)
+
+const booleanConversion = Boolean(10)
+console.log(booleanConversion)
+
+console.log(Boolean(0))
