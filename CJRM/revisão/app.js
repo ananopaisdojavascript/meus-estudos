5cd591f286f6d70bdc3b1987dcdc169658269342
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-05-22 11:54:35 -0300

    Exercícios de revisão

---
+++ CJRM/revisão/app.js
@@ -0,0 +1,226 @@
+/**
+ * Escreva um algoritmo que receba três notas e seus
+ * respectivos pesos, calcule e mostre a média ponderada
+ * entre essas notas
+ */
+
+// const nota1 = 9
+// const pesoNota1 = 2
+// const nota2 = 9
+// const pesoNota2 = 3
+// const nota3 = 10
+// const pesoNota3 = 5
+
+// const mediaPonderada = ((nota1 * pesoNota1) + (nota2 * pesoNota2) + (nota3 * pesoNota3)) / pesoNota1 + pesoNota2 + pesoNota3
+
+// console.log(mediaPonderada)
+
+/**
+ * Escreva um algoritmo que receba o salário de um funcionário,
+ * calcule e mostre o novo salário, sabendo-se que este
+ * sofreu um aumento de 25%
+ */
+
+// const salarioAtual = 2000
+// const aumento = salarioAtual * 0.25
+// const novoSalario = salarioAtual + aumento
+// console.log(novoSalario)
+
+/**
+ * Elaborar um programa que efetue a leitura de três
+ * valores (A, B, C) e apresente como resultado final
+ * a soma dos quadrados dos três valores lidos.
+ */
+
+// const A = 20
+// const B = 30
+// const C = 50
+// const soma = (A ** 2) + (B ** 2) + (C ** 2)
+// console.log(soma)
+
+/**
+ * Coloque VERDADEIRO ou FALSO na avaliação das seguintes
+ * expressões
+ */
+
+// console.log(6 <= 7)
+// console.log('Pseudocódigo' < 'PSEUDOCÓDIGO')
+// console.log((3 + 5 ) <= (2 * 4))
+// console.log((64 ** 1/2) > 5 * 2)
+// console.log('Pseudocódigo' > 'turbo')
+
+/**
+ * Qual é o resultado das operações abaixo?
+ */
+
+// console.log('mario' === 'maria')
+// console.log((10 - 4) > 7)
+// console.log((2 + 4) === 6)
+// console.log((2 * 3) > (3 * 2))
+
+/**
+ * Faça um algoritmo para calcular a área de uma 
+ * circunferência, considerando a fórmula 
+ * área = pi * raio. Utilize as variáveis AREA e RAIO, a constante
+ * pi (pi = 3.14159) e os operadores aritméticos de
+ * multiplicação
+ */
+
+// const RAIO = 50
+// const pi = 3.14159
+// const AREA = pi * RAIO
+// console.log(AREA)
+
+/*
+Leia 4 variáveis (A, B, C, D). A seguir, calcule e mostre a diferença do produto 
+de A e B pelo produto de C e D (A * B - C * D)
+*/
+
+// const A = 30
+// const B = 20
+// const C = 5
+// const D = 10
+// const diferenca = (A * B) - (C * D)
+// console.log(diferenca)
+
+/*
+Faça um algoritmo que leia quatro números informados pelo usuário e que depois 
+imprima a média ponderada, sabendo-se que os pesos são, respectivamente:
+1, 2, 3 e 4.
+*/
+
+// const A = 30
+// const B = 20
+// const C = 5
+// const D = 10
+
+// const mediaPonderada = (A * 1) + (B * 2) + (C * 3) + (D * 4) / (1 + 2 + 3 + 4)
+
+// console.log(mediaPonderada)
+
+/*
+Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela)
+o seu antecessor
+*/
+
+// const num = 10
+// const antecessor = num - 1
+// console.log(antecessor)
+
+/*
+Escreva um algoritmo para ler o número total de eleitores de um município,
+o número de votos brancos, nulos e válidos. Calcular e escrever o percentual
+que cada um representa em relação ao total de eleitores.
+*/
+
+// const votosBrancos = 1000
+// const votosNulos = 2500
+// const votosValidos = 5500
+// const quantEleitores = votosBrancos + votosNulos + votosValidos
+
+// const percVotosBrancos = (votosBrancos / quantEleitores) * 100
+// const percVotosNulos = (votosNulos / quantEleitores) * 100
+// const percVotosValidos = (votosValidos / quantEleitores) * 100
+
+// console.log(quantEleitores, percVotosBrancos, percVotosNulos, percVotosValidos)
+
+/*
+Faça um programa que peça as 4 notas bimestrais e mostre a média.
+*/
+
+// const nota1 = 7.0
+// const nota2 = 9.0
+// const nota3 = 9.5
+// const nota4 = 10
+// const media = (nota1 + nota2 + nota3 + nota4) / 4
+// console.log(media)
+
+/*
+Escreva um algoritmo que receba o preço de um produto, calcule e mostre
+o novo preço, sabendo que este sofreu um desconto de 10%.
+*/
+
+// const precoAtual = 100
+// const desconto = precoAtual * 0.10
+// const novoPreco = precoAtual - desconto
+// console.log(novoPreco)
+
+/**
+ * Faça um algoritmo que receba dois números e exiba o resultado da sua soma.
+ */
+
+// const A = 40
+// const B = 50
+// const soma = A + B
+// console.log(soma)
+
+/**
+ * Faça um algoritmo que receba dois números e ao final mostre a soma, subtração, multiplicação e a divisão dos
+números lidos.
+ */
+
+// const A = 10
+// const B = 5
+
+// const soma = A + B
+// const subtracao = A - B
+// const multiplicacao = A * B
+// const divisao = A / B
+
+// console.log(soma, subtracao, multiplicacao, divisao)
+
+/**
+ * Escrever um algoritmo que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no
+mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o
+seu nome, o salário fixo e salário no final do mês.
+ */
+
+// const salario = 3000
+// const totalVendas = 5000
+// const comissao = totalVendas * 0.15
+// const novoSalario = salario + comissao
+// console.log(novoSalario)
+
+/**
+ * Ler dois valores para as variáveis A e B, e efetuar as trocas dos valores de forma que a variável A passe a possuir o
+valor da variável B e a variável B passe a possuir o valor da variável A. Apresentar os valores trocados.
+ */
+
+// let A = 20
+// let B = 50
+// console.log(A, B)
+// let troca = 0
+// troca = A
+// A = B
+// B = troca
+// console.log(A, B)
+
+/**
+ * Ler uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. A fórmula de conversão é:
+F=(9*C+160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura em Celsius.
+ */
+
+// const tempC = 30
+// const tempF = ((9 * tempC ) + 160) / 5
+// console.log(tempF)
+
+/**
+ * Elabore um algoritmo que receba o nome do usuário e
+mostre o nome informado o número de vezes desejadas
+pelo usuário
+ */
+
+// const nome = 'Ana'
+
+// for(let i = 1; i <= 20; i++) {
+//     console.log(`${i} - ${nome}`)
+// }
+
+/**
+ * Implemente um algoritmo que leia 10 números
+inteiros e positivos
+ */
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log(i)
+// }
