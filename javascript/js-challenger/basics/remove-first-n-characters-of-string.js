f07416191c4f010f355dae0401224f9cf910ec84
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-05-22 14:35:43 -0300

    Remove first n characters of string

---
+++ javascript/js-challenger/basics/remove-first-n-characters-of-string.js
@@ -0,0 +1,9 @@
+// Write a function that takes a string (a) as argument
+// Remove the first 3 characters of a
+// Return the result
+
+const myFunction = a => a.slice(3)
+
+console.log(myFunction('abcdefg'))
+console.log(myFunction('1234'))
+console.log(myFunction('fgedcba'))
