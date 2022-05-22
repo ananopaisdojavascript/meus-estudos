48a8fdbffb5f8af80545865d4def5a770552b59b
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-05-22 14:22:36 -0300

    Comparison operators, strict equality

---
+++ javascript/js-challenger/basics/strict-equality.js
@@ -0,0 +1,9 @@
+// Write a function that takes two values, say a and b, as arguments
+// Return true if the two values are equal and of the same type
+
+const myFunction = (a, b) => a === b
+
+console.log(myFunction(2, 3))
+console.log(myFunction(3, 3))
+console.log(myFunction(1, '1'))
+console.log(myFunction('10', '10'))
