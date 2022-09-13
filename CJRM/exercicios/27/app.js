3447beda9c3e3600ac834c615565b64c911593a8
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-09-13 20:17:44 -0300

    Exercício 27

--- CJRM/exercicios/27/app.js
+++ CJRM/exercicios/27/app.js
@@ -3,19 +3,19 @@
   - Implemente um código assíncrono entre os console.log() abaixo.
 */
 
-console.log("Linha 1");
-console.log("Linha 2");
-console.log("Linha 3");
-console.log("Linha 4");
+// console.log("Linha 1");
+// console.log("Linha 2");
+// console.log("Linha 3");
+// console.log("Linha 4");
 
-setTimeout(() => {
-  console.log("Esse código só será executado após a sequência de console.log");
-}, 5000);
+// setTimeout(() => {
+//   console.log("Esse código só será executado após a sequência de console.log");
+// }, 5000);
 
-console.log("Linha 5");
-console.log("Linha 6");
-console.log("Linha 7");
-console.log("Linha 8");
+// console.log("Linha 5");
+// console.log("Linha 6");
+// console.log("Linha 7");
+// console.log("Linha 8");
 
 /*
   02
@@ -27,11 +27,12 @@ function logGreeting(name) {
   console.log(`olá, ${name}`);
 }
 
-const x = callback => {
-    return callback("Ana")
-}
+// Função de callback: função passada como argumento para outra função
+const x = (callback) => {
+  return callback("Ana");
+};
 
-x(logGreeting)
+x(logGreeting);
 
 /*
   03
@@ -50,7 +51,8 @@ console.log(lesserThanFive);
 */
 
 const prices = [12, 19, 7, 209];
-let totalPrice = prices.reduce((acc, item) => acc + item, 0);
+const getTotalPrice = (acc, price) => acc + price;
+const totalPrice = prices.reduce(getTotalPrice, 0);
 
 console.log(`Preço total: ${totalPrice}`);
 
@@ -66,7 +68,7 @@ Object.keys(car).forEach((key) => {
   car[key] = "azul";
 });
 
-console.log(car)
+console.log(car);
 
 /*
   06
@@ -78,16 +80,13 @@ console.log(car)
 */
 
 const allArguments = (a, b, c) => {
-    const verifyArguments = [a, b, c].includes(undefined);
-    if(verifyArguments) {
-        return "A função deve ser invocada com 3 argumentos"
-    } 
-        
-    return "A função foi invocada com 3 argumentos"
-    
-}
+  const verifyArguments = [a, b, c].includes(undefined);
+  return verifyArguments
+    ? "A função deve ser invocada com 3 argumentos"
+    : "A função foi invocada com 3 argumentos";
+};
 
-console.log(allArguments(1, 2, 3))
+console.log(allArguments(1, 2, 3));
 
 /*
   07
@@ -113,7 +112,49 @@ console.log(allArguments(1, 2, 3))
 let booksBox = {
   spaces: 5,
   booksIn: 0,
-  addBooks: function(number) {
-
-  }
 };
+
+const getSingularOrPlural = (quantity, singular, plural) => {
+  return quantity === 1 ? singular : plural;
+};
+
+const getAvailableSpaces = (spaces, booksIn) => {
+  const availableSpaces = spaces - booksIn;
+  const singularOrPlural = getSingularOrPlural(
+    availableSpaces,
+    "livro",
+    "livros"
+  );
+  const singularOrPluralTwo = getSingularOrPlural(
+    availableSpaces,
+    "cabe",
+    "cabem"
+  );
+
+  return `Só ${singularOrPluralTwo} mais ${availableSpaces} ${singularOrPlural}`;
+};
+
+// Criar o método para colocar os livros na caixa
+// Colocar no método o parâmetro para indicar a quantidade de livros armazenados na caixa
+booksBox.addBooks = function (bookQuantity) {
+  let { spaces } = booksBox;
+  const isTheBoxFilled = this.booksIn === spaces;
+  const hasTheBoxSpaces = this.booksIn + bookQuantity > spaces;
+  // Condição para verificar se todos os espaços da caixa não estiverem ainda preenchidos
+  if (isTheBoxFilled) {
+    return "A caixa já está cheia";
+  }
+  // Verificar se os espaços na caixa já estão ocupados
+
+  if (hasTheBoxSpaces) {
+    return getAvailableSpaces(spaces, this.booksIn)
+  }
+  // Acrescentar os livros na caixa
+  this.booksIn += bookQuantity;
+  const booksSingularOrPlural = this.booksIn === 1 ? "livro" : "livros";
+  return `Ja há ${this.booksIn} ${booksSingularOrPlural} na caixa`;
+};
+
+console.log(booksBox.addBooks(1));
+console.log(booksBox.addBooks(6))
+console.log(booksBox);
