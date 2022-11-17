b1b46c53add7d35e18fb03c7977768408247d659
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-11-17 10:04:56 -0300

    Autenticação com Azure AD

--- angular2/microsof-login/src/app/app.component.ts
+++ angular2/microsof-login/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { MsalService } from '@azure/msal-angular';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationResult } from '@azure/msal-browser';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,9 @@ import { AuthenticationResult } from '@azure/msal-browser';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
-  constructor(private msalService: MsalService) {}
+  constructor(private msalService: MsalService, private httpClient: HttpClient) {}
+
+  apiResponse: string = '';
 
   ngOnInit(): void {
     this.msalService.instance.handleRedirectPromise().then(
@@ -26,12 +29,22 @@ export class AppComponent implements OnInit{
 
   login(): void {
     this.msalService.loginRedirect();
-    // this.msalService.loginPopup().subscribe((response: AuthenticationResult) => {
-    //   this.msalService.instance.setActiveAccount(response.account)
-    // })
   }
 
   logout(): void {
     this.msalService.logout()
   }
+
+  callProfile(): unknown {
+    return this.httpClient.get('https://graph.microsoft.com/v1.0/me').subscribe(response => {
+      this.apiResponse = JSON.stringify(response);
+    });
+  }
+
+  callMessages(): unknown {
+    return this.httpClient.get('https://graph.microsoft.com/v1.0/me/messages').subscribe(response => {
+      this.apiResponse = JSON.stringify(response);
+    });
+  }
+
 }
--- angular2/microsof-login/src/app/app.module.ts
+++ angular2/microsof-login/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MsalModule, MsalService, MSAL_INSTANCE } from '@azure/msal-angular';
-import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+import { MsalInterceptor, MsalInterceptorConfiguration, MsalModule, MsalService, MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG } from '@azure/msal-angular';
+import { InteractionType, IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PublicPageComponent } from './public-page/public-page.component';
 import { PrivatePageComponent } from './private-page/private-page.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 export function MSALInstanceFactory(): IPublicClientApplication {
   return new PublicClientApplication({
@@ -15,6 +16,17 @@ export function MSALInstanceFactory(): IPublicClientApplication {
       redirectUri: 'http://localhost:4200'
     }
   })
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  // protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read']); // Prod environment. Uncomment to use.
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read', 'mail.read']);
+
+  return {
+    interactionType: InteractionType.Popup,
+    protectedResourceMap
+  };
 }
 
 @NgModule({
@@ -26,14 +38,24 @@ export function MSALInstanceFactory(): IPublicClientApplication {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MsalModule
+    MsalModule,
+    HttpClientModule
   ],
   providers: [
     {
       provide: MSAL_INSTANCE,
       useFactory: MSALInstanceFactory
     },
-    MsalService
+    MsalService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    }
   ],
   bootstrap: [AppComponent]
 })
